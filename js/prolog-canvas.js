1e79b89cabf5f3c927a381b9fad88a970cc22b4e
kfrackiewicz <lopek1985@gmail.com>
2015-07-11 11:36:12 +0200

    rm global watcher

--- js/prolog-canvas.js
+++ js/prolog-canvas.js
@@ -41,7 +41,6 @@ Prolog.prototype.createCanvas = function () {
     'use strict';
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
-    window.ctx = this.ctx;
     this.setCanvasSize();
 }
 Prolog.prototype.bindEvents = function () {
